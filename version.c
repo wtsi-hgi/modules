5637af68b654f65c4f80920a042802355a69704b
Joshua Randall <joshua.randall@sanger.ac.uk>
2013-10-09 23:56:35 +0100

    revert changes to version.c for patching

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.3.b";
-const char *date_string = "2013-10-07";
-/* NEW TAG "modules-3-3-b" */
-/* OLD TAG "modules-3-3-a" */
+const char *version_string = "3.3.a";
+const char *date_string = "2009-08-04";
+/* NEW TAG "modules-3-3-a" */
+/* OLD TAG "modules-3-2-7" */
